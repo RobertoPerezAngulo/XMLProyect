545a2fb8a6fbbd59b6b6952059f6a38a8266c480
Jose Roberto <jrperez@grupoautofin.com>
2022-03-31 10:45:50 -0600

    Primer version

---
+++ ConsoleApplication1/ConsoleApplication1/Models/XmlCFDI.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ConsoleApplication1.Models
+{
+    public class ENCABEZADO_EMISOR
+    {
+        public string EMPRESA;
+        public string RFC_EMPRESA;
+        public string REG_PATRONAL;
+        public string DOM_EMPRESA;
+        public string DOM_LUGAR_TRABAJO;
+        public int REG_FISCAL;
+        public string LUG_EXPEDICION;
+        public string TIPO_COMPROBANTE;
+        public string UNIDAD;
+    }
+
+    public class ENCABEZADO
+    {
+        public string FOLIO_FISCAL;
+        public DateTime FECHA_EMISION;
+        public DateTime FECHA_TIMBRADO;
+        public string CERTIFICADO_EMISOR;
+        public double CERTIFICADO_SAT;
+    }
+
+    public class ENCABEZADO_RECEPTOR
+    {
+        public string NOM_EMPLEADO;
+        public string PUESTO;
+        public string RFC_EMPLEADO;
+        public string CURP_EMPLEADO;
+        public double NSS;
+        public string FEC_INGRESO;
+        public string DEPARTAMENTO;
+        public int CVE_EMPLEADO;
+        public string TIPO_NOMINA;
+        public double SALARIO_DIARIO;
+        public double SDI_IMSS;
+        public double SDP;
+        public int DIAS_LABORADOS;
+        public string PER_PAGO;
+    }
+
+    public class CONCEPTOS
+    {
+        public int CVE_CONCEPTO;
+        public string DES_CONCEPTO;
+        public int UNIDADES;
+        public double PERCEPCIONES;
+        public int DEDUCCIONES;
+    }
+
+    public class DETALLE
+    {
+        public List<CONCEPTOS> CONCEPTOS;
+    }
+
+    public class TOTAL
+    {
+        public double TOTAL_PERCEPCION;
+        public double TOTAL_DEDUCCION;
+        public double NETO_PAGAR;
+        public string NETO_LETRA;
+    }
+
+    public class TOTAL_QR
+    {
+        public double NETO_PAGAR_QR;
+    }
+
+    public class PRESTACIONES_ESPECIE
+    {
+        public CONCEPTOS CONCEPTOS;
+    }
+
+    public class PIE_PAGINA_1
+    {
+        public int CUENTA_DEPOSITO;
+        public string BANCO_NOMBRE;
+        public string LEYENDA_ESPECIE;
+        public PRESTACIONES_ESPECIE PRESTACIONES_ESPECIE;
+        public int TOTAL_PERCEPCION_ESPECIE;
+        public int TOTAL_DEDUCCION_ESPECIE;
+        public int NETO_PAGAR_ESPECIE;
+    }
+
+    public class PIE_PAGINA_2
+    {
+        public string LEYENDA;
+    }
+
+    public class PIE_PAGINA_3
+    {
+        public string SELLO_CONTRIBUYENTE;
+        public string SELLO_SAT;
+        public string CADENA_ORIGINAL;
+        public DateTime FECHA_PAGA_LEYENDA;
+        public string NOM_EMPLEADO_LEYENDA;
+    }
+
+    public class RECIBO_NOMINA_CFDI
+    {
+        public ENCABEZADO_EMISOR ENCABEZADO_EMISOR;
+        public ENCABEZADO ENCABEZADO;
+        public ENCABEZADO_RECEPTOR ENCABEZADO_RECEPTOR;
+        public DETALLE DETALLE;
+        public TOTAL TOTAL;
+        public TOTAL_QR TOTAL_QR;
+        public PIE_PAGINA_1 PIE_PAGINA_1;
+        public PIE_PAGINA_2 PIE_PAGINA_2;
+        public PIE_PAGINA_3 PIE_PAGINA_3;
+    }
+
+    public class RECIBO_NOMINA
+    {
+        public RECIBO_NOMINA_CFDI RECIBO_NOMINA_CFDI;
+    }
+
+
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,72 @@
+﻿using ConsoleApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Web;
+using PdfSharp.Drawing;
+using System.Drawing;
+using ConsoleApplication1.Rpt;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string path = @"C:\Users\jrperez\Documents\visual studio 2015\Projects\ConsoleApplication1\ConsoleApplication1\38632_15032022_Formato2.xml";
+                string _xmlNomina = @"C:\Users\jrperez\Documents\visual studio 2015\Projects\ConsoleApplication1\ConsoleApplication1\Recursos\XMLNomina.html";
+                XmlSerializer serializer = new XmlSerializer(typeof(RECIBO_NOMINA));
+                var doc1 = File.ReadAllText(path);
+                var _xml = File.ReadAllText(_xmlNomina);
+                RECIBO_NOMINA oNomina =  (RECIBO_NOMINA)serializer.Deserialize(new StringReader(doc1));
+                //var htmlToPdf = new NReco.PdfGenerator.HtmlToPdfConverter();
+                //htmlToPdf.Orientation = NReco.PdfGenerator.PageOrientation.Portrait;
+                //htmlToPdf.Zoom = 3f;
+                //htmlToPdf.Size = NReco.PdfGenerator.PageSize.A3;
+                //htmlToPdf.GeneratePdf(_xml, null, @"C:\Users\jrperez\Documents\visual studio 2015\Projects\ConsoleApplication1\ConsoleApplication1\mapa.pdf");
+
+
+                Nomina rpt = new Nomina();
+                rpt.SetParameterValue("NOMBRE_PAGADORA", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_EMISOR.EMPRESA);
+                rpt.SetParameterValue("REGI_PAT", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_EMISOR.REG_PATRONAL);
+                rpt.SetParameterValue("RFC", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_EMISOR.RFC_EMPRESA);
+                rpt.SetParameterValue("DOMICILIO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_EMISOR.DOM_EMPRESA.Replace("|"," "));
+                rpt.SetParameterValue("REGIMEN_FISCAL", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_EMISOR.REG_FISCAL);
+                rpt.SetParameterValue("LUGAR_DE_EXPE", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_EMISOR.LUG_EXPEDICION);
+                rpt.SetParameterValue("TIPO_COMPRO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_EMISOR.TIPO_COMPROBANTE);
+                rpt.SetParameterValue("FOLIO_FISCAL", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO.FOLIO_FISCAL);
+                rpt.SetParameterValue("FECHA_EMI", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO.FECHA_EMISION);
+                rpt.SetParameterValue("FECHA_TIMBRA", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO.FECHA_TIMBRADO);
+                rpt.SetParameterValue("CERTIFICADO_EMI", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO.CERTIFICADO_EMISOR);
+                rpt.SetParameterValue("CERTIFICADO_SAT", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO.CERTIFICADO_SAT.ToString());
+                rpt.SetParameterValue("NOMBRE_EMPLEADO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.NOM_EMPLEADO);
+                rpt.SetParameterValue("PUESTO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.PUESTO);
+                rpt.SetParameterValue("RFC_EMPLEADO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.RFC_EMPLEADO);
+                rpt.SetParameterValue("CLAVE_EMPLEADO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.CVE_EMPLEADO);
+                rpt.SetParameterValue("CURP", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.CURP_EMPLEADO);
+                rpt.SetParameterValue("NSS", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.NSS);
+                rpt.SetParameterValue("FECHA_INGRESO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.FEC_INGRESO);
+                rpt.SetParameterValue("DEPARTAMENTO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.DEPARTAMENTO);
+                rpt.SetParameterValue("TIPO_NOMINA", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.TIPO_NOMINA);
+                rpt.SetParameterValue("SALARIO_DIARIO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.SALARIO_DIARIO);
+                rpt.SetParameterValue("SDI_IMSS", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.SDI_IMSS);
+                rpt.SetParameterValue("DIAS_LABO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.DIAS_LABORADOS);
+                rpt.SetParameterValue("PERIODO_PAGO", oNomina.RECIBO_NOMINA_CFDI.ENCABEZADO_RECEPTOR.PER_PAGO);
+                rpt.ExportToDisk(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat, @"C:\Users\jrperez\Documents\visual studio 2015\Projects\ConsoleApplication1\ConsoleApplication1\cheetos.pdf");
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+        }
+    }
+}
